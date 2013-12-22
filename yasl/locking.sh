f491ac89c1cccdbd68156837b485b88f0619e49b
Oleg Sharoyko <ols@google.com>
2013-12-22 16:27:15 +0000

    Use mktemp to create temporary file. Quote variables.

--- yasl/locking.sh
+++ yasl/locking.sh
@@ -8,6 +8,8 @@ fi
 lock() {
   local _lockfile
   local _non_blocking
+  local _pid
+  local _tmpfile
 
   # options parsing
   # TODO(osharoiko): maybe move into separate function
@@ -30,7 +32,7 @@ lock() {
     shift
   done
 
-  _lockfile=$1
+  _lockfile="$1"
 
   if [ -z "${_lockfile}" ]; then
     echo "lock: lockfile not specified"
@@ -38,14 +40,17 @@ lock() {
   fi
 
   rc=0
-  # TODO(osharoiko): use safe mktemp
-  echo $$ > ${_lockfile}.$$
-  while ! ln ${_lockfile}.$$ ${_lockfile} 1>/dev/null 2>&1; do
-    pid=`cat ${_lockfile} 2>/dev/null`
-    if ! kill -0 $pid 1>/dev/null 2>&1; then
+  _tmpfile=$(mktemp "${_lockfile}.XXXXXXXX")
+  echo $$ > "${_tmpfile}"
+  while ! ln "${_tmpfile}" "${_lockfile}" 1>/dev/null 2>&1; do
+    _pid=$(cat "${_lockfile}" 2>/dev/null)
+    if [ -z "${_pid}" ]; then
+      # TODO(osharoiko): add a log record here
+      # informing about existing lockfile without pid
+    elif ! kill -0 "${_pid}" 1>/dev/null 2>&1; then
       # TODO(osharoiko): add a log record here
       # informing about stale lockfile
-      if ! rm -f ${_lockfile} 1>/dev/null 2>&1; then
+      if ! rm -f "${_lockfile}" 1>/dev/null 2>&1; then
         echo "lock: couldn't delete unused ${_lockfile}"
         rc=255
         break
@@ -59,8 +64,8 @@ lock() {
     fi
   done
 
-  rm -f ${_lockfile}.$$ 1>/dev/null 2>&1
-  return $rc
+  rm -f "${_tmpfile}" 1>/dev/null 2>&1
+  return ${rc}
 }
 
 unlock() {
@@ -84,12 +89,12 @@ unlock() {
     shift
   done
 
-  _lockfile=$1
+  _lockfile="$1"
 
   if [ -z "${_lockfile}" ]; then
     echo "unlock: lockfile not specified"
     return 255
   fi
 
-  rm -f ${_lockfile} 1>/dev/null 2>&1
+  rm -f "${_lockfile}" 1>/dev/null 2>&1
 }
